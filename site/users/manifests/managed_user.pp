e439db0c27481eb988688c47911ed983b6b5e8aa
Dzmitry Vadalazhski <dv@dv.com>
2016-10-14 14:03:23 -0400

    defined resource types

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,20 @@
+define user::managed_user (
+	$homedir = "/home/${title}",
+	$password = 'test',
+	$shell = '/bin/bash',
+) {
+  user { $title:
+    ensure           => present,
+    home             => $homedir,
+    password         => $password,
+    shell            => $shell,
+  }
+  file { $homedir:
+	ensure => directory,
+	require => User[$title],
+  }
+  file { "${homedir}/.ssh":
+	ensure => directory,
+	require => File[$homedir],
+  }
+}
